77932515267b8aea18840ffae01de28af43358ed
Szymon Roszyk <roszyk.szymon@gmail.com>
2023-01-18 20:52:11 +0100

    stoper init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sda-app';
+  licznik = 0;
+
+  constructor() {
+    setInterval(() => {
+      this.licznik++;
+      console.log(this.licznik);
+    }, 100);
+  }
 }
