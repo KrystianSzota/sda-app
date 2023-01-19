3f5d722d3e2cd75c4c4091e1de14489b5ec44706
Szymon Roszyk <roszyk.szymon@gmail.com>
2023-01-19 18:33:49 +0100

    stop stoper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   seconds = 0;
   decySeconds = 0;
+  private intervalId: any = undefined;
 
   constructor() {}
 
   startStoper(): void {
-    setInterval(() => {
+    this.intervalId = setInterval(() => {
       this.decySeconds++;
       if (this.decySeconds === 10) {
         this.seconds++;
         this.decySeconds = 0;
       }
     }, 100);
+  }
+
+  stopStoper(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 }
