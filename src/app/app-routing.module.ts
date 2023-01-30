26e1a5f88f9c15835460fc0afd8109d0c3f32180
Szymon Roszyk <roszyk.szymon@gmail.com>
2023-01-30 20:55:44 +0100

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RoundsListComponent } from './rounds-list/rounds-list.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
-const routes: Routes = [{ path: 'rounds', component: RoundsListComponent }];
+const routes: Routes = [
+  { path: 'rounds', component: RoundsListComponent },
+  { path: 'users', component: UsersListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
