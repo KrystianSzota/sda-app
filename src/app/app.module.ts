40da5e25425d43d30f88080bb53f0e98fb01f0bf
Szymon Roszyk <roszyk.szymon@gmail.com>
2023-01-30 18:51:20 +0100

    Users list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { RoundsListComponent } from './rounds-list/rounds-list.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoundsListComponent
+    RoundsListComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
